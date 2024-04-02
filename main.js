83b5302a04fee8854e6fbdb978b0951586e1fae7
adil5679 <malikadil5679@gmail.com>
2024-04-02 23:39:32 +0500

    compeleted

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const prompt = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter your First Number ?"
+    },
+    {
+        name: "operator",
+        message: "Select Operator by given operators",
+        type: "list",
+        choices: ["-", "+", "*", "/"]
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter your First Number ?"
+    }
+]);
+if (prompt.operator === "+") {
+    console.log(`\n Your Answer Is This:${prompt.num1 + prompt.num2}`);
+}
+else if (prompt.operator === "*") {
+    console.log(`\n Your Answer Is This:${prompt.num1 * prompt.num2}`);
+}
+else if (prompt.operator === "/") {
+    console.log(`\n Your Answer Is This:${prompt.num1 / prompt.num2}`);
+}
+else if (prompt.operator === "-") {
+    console.log(`\n Your Answer Is This:${prompt.num1 - prompt.num2}`);
+}
+else {
+    console.log("invalid operator");
+}
